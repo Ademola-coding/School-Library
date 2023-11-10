b08ff889670f1fcc8f201a4b6e2276ae7b8f1f8d
Ademola Owoeye <ademolaowoeye07@gmail.com>
2023-11-10 14:10:53 +0100

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,17 +1,16 @@
 class Person
-  attr_accessor :name, :age, :id, :parent_permission, :rentals
+  attr_accessor :name, :age, :id
 
   def initialize(age, parent_permision: true, name: 'maximilianus', id: Random.rand(1..1000))
     super()
-    @id = id
+    @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permision = parent_permision
-    @rentals = []
   end
 
   def can_use_services?
-    of_age? && @parent_permision
+    of_age?() || @parent_permision
   end
 
   def correct_name
@@ -26,7 +25,7 @@ class Person
   private
 
   def of_age?
-    @age > 18
+    @age >= 18
   end
 end
 
